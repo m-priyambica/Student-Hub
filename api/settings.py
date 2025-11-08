cf300b0856f22abf6a724b0d44810b93131f5e9c
m-priyambica <priyambica25@gmail.com>
2025-11-09 03:35:08 +0530

    Feat(users): Register User model in admin panel

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users'
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# In api/settings.py (at the very bottom)
+
+# Tell Django to use our custom User model
+AUTH_USER_MODEL = 'users.User'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,40 @@
+# In users/admin.py
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    """
+    A custom admin class for our User model.
+    """
+    
+    # We add our custom fields to the 'fieldsets'.
+    # This controls the layout of the 'Edit User' page.
+    # We add our 'Personal Info' section.
+    fieldsets = (
+        *UserAdmin.fieldsets,  # Inherits all the default fields
+        (
+            'Personal Info',  # Title of our new section
+            {
+                'fields': (
+                    'full_name',
+                    'is_email_verified',
+                    'secret_question',
+                    'secret_answer',
+                )
+            }
+        )
+    )
+
+    # This controls the columns displayed in the user list.
+    list_display = (
+        'username', 
+        'email', 
+        'full_name', 
+        'is_staff', 
+        'is_email_verified'
+    )
 
 # Register your models here.
+admin.site.register(User, CustomUserAdmin)
