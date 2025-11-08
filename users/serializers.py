1803f9579ce0ae833025f93a57151f3e7abc0298
m-priyambica <priyambica25@gmail.com>
2025-11-09 04:19:17 +0530

    Feat(users): Add custom validation for @stanley.edu.in emails

---
+++ users/serializers.py
@@ -0,0 +1,52 @@
+# In users/serializers.py (UPDATED)
+
+from rest_framework import serializers
+from .models import User
+import re # Import the regular expressions module
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    """
+    A serializer for registering new users.
+    We only define the fields needed for creation.
+    """
+    
+    class Meta:
+        model = User
+        # List the fields you want to accept for registration
+        fields = ('username', 'email', 'full_name', 'password')
+        
+        extra_kwargs = {
+            'password': {
+                'write_only': True, # This means the password won't be sent back in the response
+                'style': {'input_type': 'password'} # This hides it in the browsable API
+            }
+        }
+    
+    # --- THIS IS OUR NEW CUSTOM VALIDATION ---
+    def validate_email(self, value):
+        """
+        Check that the email is from the allowed college domain.
+        """
+        # We can make this more flexible in case there are subdomains, etc.
+        # This regex checks for "anything@stanley.edu.in"
+        # The re.IGNORECASE makes it case-insensitive.
+        if not re.match(r"^[A-Za-z0-9._%+-]+@stanley\.edu\.in$", value, re.IGNORECASE):
+            raise serializers.ValidationError("Only emails from @stanley.edu.in are allowed.")
+        
+        return value
+    # --- END OF NEW VALIDATION ---
+    
+    def create(self, validated_data):
+        """
+        This method is called when we save the serializer (e.g., serializer.save()).
+        We override it to use our custom create_user method
+        which properly hashes the password.
+        """
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            full_name=validated_data['full_name'],
+            password=validated_data['password']
+        )
+        
+        return user
